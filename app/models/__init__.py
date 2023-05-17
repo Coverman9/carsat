bfc15ff9f3a45cd73c5ffbd9dc44abf93eaa2e03
Coverman9 <usubalieve@gmail.com>
2023-05-17 01:37:54 -0500

    fixed seed code

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,7 @@
-from .db import db
+from .db import db, environment, SCHEMA
 from .user import User
-from .db import environment, SCHEMA
+from .car import Car
+from .image import Image
+from .review import Review
+from .test_drive import Testdrive
+from .wishlist import Wishlist
--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,9 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .cars import seed_cars, undo_cars
+from .reviews import seed_reviews, undo_reviews
+from .test_drives import seed_testdrives, undo_testdrives
+from .wishlists import seed_wishlists, undo_wishlists
 
 from app.models.db import db, environment, SCHEMA
 
@@ -12,17 +16,30 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_wishlists()
+        undo_reviews()
+        undo_testdrives()
+        undo_cars()
         undo_users()
+
     seed_users()
+    seed_cars()
+    seed_reviews()
+    seed_testdrives()
+    seed_wishlists()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_wishlists()
+    undo_reviews()
+    undo_testdrives()
+    undo_cars()
     undo_users()
     # Add other undo functions here
