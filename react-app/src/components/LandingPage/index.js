23e9edc5d8bef7655fdba183f35642fa3f6706f6
Coverman9 <usubalieve@gmail.com>
2023-05-21 23:45:17 -0500

    video deleted

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,10 +1,8 @@
-import video from "./video-1.mp4";
 import "./LandingPage.css";
 const LandingPage = () => {
   return (
     <>
       <div className="hero-container">
-        <video src={video} autoPlay loop muted />
         <h1>GET NEW CAR!</h1>
         <p>What are you waiting for?</p>
         <div className="hero-btns">
@@ -15,85 +13,7 @@ const LandingPage = () => {
         <h2>Check out these EPIC Unaas</h2>
         <div className="cards__container">
           <div className="cards__wrapper">
-            <ul className="cards__items">
-              <li className="cards__item">
-                <figure
-                  className="cards__item__pic-wrap"
-                  data-category="Mercedes"
-                >
-                  <img
-                    className="cards__item__img"
-                    alt="img"
-                    src="https://carloson.ru/uploads/images/cars/62/5517804b683b93f62d6c03612223a1bd-740x507.jpg"
-                  />
-                </figure>
-                <div className="cards__item__info">
-                  <h5 className="cards__item__text">Hello</h5>
-                </div>
-              </li>
-              <li className="cards__item">
-                <figure
-                  className="cards__item__pic-wrap"
-                  data-category="Mercedes"
-                >
-                  <img
-                    className="cards__item__img"
-                    alt="img"
-                    src="https://carloson.ru/uploads/images/cars/62/5517804b683b93f62d6c03612223a1bd-740x507.jpg"
-                  />
-                </figure>
-                <div className="cards__item__info">
-                  <h5 className="cards__item__text">Hello</h5>
-                </div>
-              </li>
-            </ul>
-            <ul className="cards__items">
-              <li className="cards__item">
-                <figure
-                  className="cards__item__pic-wrap"
-                  data-category="Mercedes"
-                >
-                  <img
-                    className="cards__item__img"
-                    alt="img"
-                    src="https://carloson.ru/uploads/images/cars/62/5517804b683b93f62d6c03612223a1bd-740x507.jpg"
-                  />
-                </figure>
-                <div className="cards__item__info">
-                  <h5 className="cards__item__text">Hello</h5>
-                </div>
-              </li>
-              <li className="cards__item">
-                <figure
-                  className="cards__item__pic-wrap"
-                  data-category="Mercedes"
-                >
-                  <img
-                    className="cards__item__img"
-                    alt="img"
-                    src="https://carloson.ru/uploads/images/cars/62/5517804b683b93f62d6c03612223a1bd-740x507.jpg"
-                  />
-                </figure>
-                <div className="cards__item__info">
-                  <h5 className="cards__item__text">Hello</h5>
-                </div>
-              </li>
-              <li className="cards__item">
-                <figure
-                  className="cards__item__pic-wrap"
-                  data-category="Mercedes"
-                >
-                  <img
-                    className="cards__item__img"
-                    alt="img"
-                    src="https://carloson.ru/uploads/images/cars/62/5517804b683b93f62d6c03612223a1bd-740x507.jpg"
-                  />
-                </figure>
-                <div className="cards__item__info">
-                  <h5 className="cards__item__text">Hello</h5>
-                </div>
-              </li>
-            </ul>
+           
           </div>
         </div>
       </div>
