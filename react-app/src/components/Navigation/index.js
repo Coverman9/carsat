84de3b0f2201d1e1af2dd32da1660008379cf55d
Coverman9 <usubalieve@gmail.com>
2023-05-23 11:39:41 -0500

    a lot of css fixed

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -4,40 +4,37 @@ import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import { Link } from "react-router-dom";
 import "./Navigation.css";
+import logo from "./carsat-logo.png";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
 
   return (
     <div className="main-navbar">
-        <div>
-          <li>
-            <NavLink exact to="/">
-              Home
-            </NavLink>
-          </li>
-        </div>
-        {isLoaded && (
-          <>
-            <div className="navbar-profile-buttons">
-              {sessionUser && (
-                <>
-                  <Link to={`/profile/${sessionUser.id}`}>
-                    <button className="profile-button">Profile</button>
-                  </Link>
-                  <Link to="/cars/new">
-                    <button className="addcar-button">Add a Car</button>
-                  </Link>
-                </>
-              )}
-            </div>
-            <div>
-              <li>
-                <ProfileButton user={sessionUser} />
-              </li>
-            </div>
-          </>
-        )}
+      <div className="logo-carsat">
+        <NavLink exact to="/">
+          <img className="carsat-logo" src={logo}/>
+        </NavLink>
+      </div>
+      {isLoaded && (
+        <>
+          <div className="navbar-profile-buttons">
+            {sessionUser && (
+              <>
+                <Link to={`/profile/${sessionUser.id}`}>
+                  <button className="profile-button">Profile</button>
+                </Link>
+                <Link to="/cars/new">
+                  <button className="addcar-button">Add a Car</button>
+                </Link>
+              </>
+            )}
+          </div>
+          <div>
+            <ProfileButton user={sessionUser} />
+          </div>
+        </>
+      )}
     </div>
   );
 }
